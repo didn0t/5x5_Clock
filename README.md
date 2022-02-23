5fb82741f7222629eb3742901113030c142840ac
Andy Piper <andypiper@users.noreply.github.com>
2022-02-23 10:59:32 +0000

    set hostname, add video link

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-Simple single digit clock based on a PV Electronics single digit Nixie clock design.
+# 5x5 Clock
 
-For use on ESP32c3 Board with 5x5 Pixel display - https://uk.banggood.com/ESP32-C3-Development-Board-RISC-V-WiFi-Bluetooth-IoT-Development-Board-Compatible-with-Python-p-1914005.html
+Simple single digit clock display based on a PV Electronics single digit Nixie clock design.
+
+For use on ESP32c3 Board with 5x5 Pixel display - available [here](https://uk.banggood.com/ESP32-C3-Development-Board-RISC-V-WiFi-Bluetooth-IoT-Development-Board-Compatible-with-Python-p-1914005.html)
 
 Displays date in DDMMYY format every minute.
 
-Uses ezTime module for NTP and font based on https://jared.geek.nz/2014/jan/custom-fonts-for-microcontrollers.
+Uses ezTime module for NTP, font based on https://jared.geek.nz/2014/jan/custom-fonts-for-microcontrollers.
+
+[Video on YouTube](https://www.youtube.com/watch?v=eF4v-Na2XIM)
 
 Future ideas include:
 
--- c3.ino
+++ c3.ino
@@ -11,42 +11,48 @@
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-uint32_t red    = strip.Color(255, 0, 0);
-uint32_t green  = strip.Color(0, 255, 0);
-uint32_t blue   = strip.Color(0, 0, 255);
-uint32_t amber   = strip.Color(255, 140, 0);
+uint32_t red = strip.Color(255, 0, 0);
+uint32_t green = strip.Color(0, 255, 0);
+uint32_t blue = strip.Color(0, 0, 255);
+uint32_t amber = strip.Color(255, 140, 0);
 
 Timezone myTZ;
 
-
-void DrawPixel(uint32_t colour, uint8_t x, uint8_t y, uint8_t brightness) {
+void DrawPixel(uint32_t colour, uint8_t x, uint8_t y, uint8_t brightness)
+{
   uint8_t led = (4 - y) * 5 + (4 - x); //  Calc LED from x,y coords, 0,0 is bottom left pixel
-  uint8_t red =   ((colour >> 16) & 0xff) * brightness / 100;
-  uint8_t green = ((colour >> 8 ) & 0xff) * brightness / 100;
-  uint8_t blue =  (colour & 0xff) * brightness / 100;
+  uint8_t red = ((colour >> 16) & 0xff) * brightness / 100;
+  uint8_t green = ((colour >> 8) & 0xff) * brightness / 100;
+  uint8_t blue = (colour & 0xff) * brightness / 100;
   strip.setPixelColor(led, red, green, blue);
-  //strip.show();
+  // strip.show();
 }
 
-
 // Based on code from https://jared.geek.nz/2014/jan/custom-fonts-for-microcontrollers
-void DrawChar(uint32_t colour, char c, uint8_t brightness) {
+void DrawChar(uint32_t colour, char c, uint8_t brightness)
+{
   uint8_t x, y;
 
   // Convert the character to an index
   c = c & 0x7F;
-  if (c < ' ') {
+  if (c < ' ')
+  {
     c = 0;
-  } else {
+  }
+  else
+  {
     c -= ' ';
   }
 
   // 'font' is a multidimensional array of [96][char_width]
-  const uint8_t* chr = font[c];
+  const uint8_t *chr = font[c];
   // Draw pixels
-  for (x = 0; x < CHAR_WIDTH; x++) {
-    for (y = 0; y < CHAR_HEIGHT; y++) {
-      if (chr[x] >> 2 & (1 << y)) {
+  for (x = 0; x < CHAR_WIDTH; x++)
+  {
+    for (y = 0; y < CHAR_HEIGHT; y++)
+    {
+      if (chr[x] >> 2 & (1 << y))
+      {
         DrawPixel(colour, x, y, brightness);
       }
       strip.show();
@@ -54,14 +60,16 @@ void DrawChar(uint32_t colour, char c, uint8_t brightness) {
   }
 }
 
-
-void FadeChar(uint32_t colour, char c) {
-  for (uint8_t br = 20; br <= 100; br = br + 5) {
+void FadeChar(uint32_t colour, char c)
+{
+  for (uint8_t br = 20; br <= 100; br = br + 5)
+  {
     DrawChar(colour, c, br);
   }
   delay(100);
 
-  for (uint8_t br = 100; br > 0; br = br - 5) {
+  for (uint8_t br = 100; br > 0; br = br - 5)
+  {
     DrawChar(colour, c, br);
   }
   delay(100);
@@ -70,38 +78,46 @@ void FadeChar(uint32_t colour, char c) {
   strip.show(); // Initialize all pixels to 'off'
 }
 
-
-void FadeString(uint32_t colour, String s) {
+void FadeString(uint32_t colour, String s)
+{
   char buffer[s.length() + 1];
   s.toCharArray(buffer, s.length() + 1);
   for (int i = 0; i < s.length() + 1; i++)
     FadeChar(colour, buffer[i]);
 }
 
-
-
-void GetNTP() {
+void GetNTP()
+{
 
   WiFi.mode(WIFI_STA);
   WiFi.setTxPower(WIFI_POWER_5dBm);
+  WiFi.setHostname("esp32clock");
   WiFiManager wm;
   wm.setDebugOutput(false);
 
+  // TODO: probably should enable the user to select a timezone
+  // and set a hostname on the Wifi manager setup screen
   bool res;
   res = wm.autoConnect("5x5_Clock", "Clock123"); // create password protected ap
-  if (!res) {
+  if (!res)
+  {
     Serial.println(F("Failed to start Wifi Manager AP"));
-  } else {
+  }
+  else
+  {
     Serial.println(F("[+] Connected to Wi-Fi"));
     Serial.println(F("[+] Syncing NTP"));
     waitForSync();
     Serial.println("[+] UTC: " + UTC.dateTime());
 
-    if (myTZ.setLocation(F("Europe/London"))) {
+    if (myTZ.setLocation(F("Europe/London")))
+    {
       Serial.println(F("[+] Timezone lookup OK"));
       Serial.print("[+] UK: ");
       Serial.println(myTZ.dateTime());
-    } else {
+    }
+    else
+    {
       Serial.println("[-] Timezone lookup failed, will use UTC");
       myTZ = UTC;
     }
@@ -109,8 +125,8 @@ void GetNTP() {
   WiFi.disconnect();
 }
 
-
-void setup() {
+void setup()
+{
 
   strip.begin();
   strip.setBrightness(20);
@@ -121,14 +137,15 @@ void setup() {
   FadeString(green, "Up");
 }
 
-void loop() {
+void loop()
+{
 
   events();
-  
-  if (minuteChanged()) {
+
+  if (minuteChanged())
+  {
     FadeString(blue, myTZ.dateTime("dmy"));
   }
 
   FadeString(amber, myTZ.dateTime("H:i"));
-
 }
